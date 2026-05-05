785f84980c605331d8b86d7abe9d2c5d92365e4d
Juan Bossio <Juan@DESKTOP>
2026-05-05 12:37:03 -0300

    Agrego SessionManager

---
+++ DAL/UsuarioDAL_62_BP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+namespace DAL
+{
+    public class UsuarioDAL_62_BP
+    {
+        private Acceso_62_BP _acceso = new Acceso_62_BP();
+
+        public DataTable BuscarUsuarioPorNombre(string nombre) {
+            string query = "SELECT id, nombre, contrasenaHasheada, intentosLogin, rol FROM Usuario WHERE nombre = @nombre";
+            SqlParameter[] parametros = new SqlParameter[]
+            {
+                new SqlParameter("@nombre", nombre)
+            };
+            DataTable tabla = _acceso.leer(query, parametros);
+            
+            return tabla;
+        }
+    }
+}
---
+++ SEG/SessionManager_62_BP.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace SEG
+{
+    public class SessionManager_62_BP
+    {
+        private static SessionManager_62_BP instancia;
+
+        public static SessionManager_62_BP GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new SessionManager_62_BP();
+            }
+            return instancia;
+        }
+
+        private Usuario_62_BP usuarioLogueado;
+
+		public Usuario_62_BP UsuarioLogueado
+        {
+			get { return usuarioLogueado; }
+			set { usuarioLogueado = value; }
+		}
+
+        public void Login(Usuario_62_BP usuario)
+        {
+            if (usuarioLogueado == null)
+            {
+                try
+                {
+                    UsuarioDAL_62_BP usuarioDal = new UsuarioDAL_62_BP();
+                    var tabla = usuarioDal.BuscarUsuarioPorNombre(usuario.Nombre);
+                    Usuario_62_BP usuarioEncontrado = new Usuario_62_BP();
+                    foreach (DataRow fila in tabla.Rows)
+                    {
+                        usuarioEncontrado.Id = Convert.ToInt32(fila["id"]);
+
+                        usuarioEncontrado.Nombre = fila["nombre"].ToString();
+
+                        usuarioEncontrado.ContrasenaHasheada = fila["contrasenaHasheada"].ToString();
+
+                        usuarioEncontrado.IntentosLogin = Convert.ToInt32(fila["intentosLogin"]);
+
+                        usuarioEncontrado.Rol = Convert.ToInt32(fila["rol"]);
+                    }
+                    if (usuarioEncontrado.ContrasenaHasheada == usuario.ContrasenaHasheada)
+                    {
+                        usuarioLogueado = usuario;
+                    }
+                }
+                catch (Exception ex) {
+                    new Exception (ex.ToString());
+                }
+                
+            }
+        }
+
+        public void Logout()
+        {
+            usuarioLogueado = null;
+        }
+    }
+}
