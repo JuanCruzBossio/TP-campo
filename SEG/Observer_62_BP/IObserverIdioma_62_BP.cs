e2bac82560d1eb23d5c580541e737c0aeaf939a6
PAriel98 <ariel.pavon98@gmail.com>
2026-06-22 20:49:00 -0300

    Creacion del observer y la clase de idiomas

---
+++ SEG/Observer_62_BP/IObserverIdioma_62_BP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SEG_62_BP.Observer
+{
+    
+    public interface IObservadorIdioma_62_BP
+    {
+        void ActualizarIdioma_62_BP(Idioma_62_BP idioma_62_BP);
+    }
+}
---
+++ SEG/Observer_62_BP/Idioma_62_BP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SEG_62_BP.Observer
+{
+   
+    public class Idioma_62_BP
+    {
+      
+        public string Nombre_62_BP { get; set; }
+
+        public Dictionary<string, string> Traducciones_62_BP { get; set; }
+      
+        public Idioma_62_BP()
+        {
+            Nombre_62_BP = string.Empty;
+            Traducciones_62_BP = new Dictionary<string, string>();
+        }
+
+        public Idioma_62_BP(string nombre_62_BP, Dictionary<string, string> traducciones_62_BP)
+        {
+            Nombre_62_BP = nombre_62_BP;
+            Traducciones_62_BP = traducciones_62_BP ?? new Dictionary<string, string>();
+        }
+    }
+}
